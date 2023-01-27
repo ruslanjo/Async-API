2cecd145e805e548c9d362f35cd65459acfb8d16
Ruslan Tarutin <tarutin.ruslan99@gmail.com>
2023-01-28 01:55:56 +0300

    Codereview 10, 11, 15: [added pagination to persons.py, added FastAPI documentation via summary attribute]

--- src/api/v1/persons.py
+++ src/api/v1/persons.py
@@ -3,27 +3,20 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 
+from core.errors_text import http_errors
+from .models import FilmShortModel, Person
 from services.persons import PersonService, get_person_service
 
 router = APIRouter()
 
 
-class Person(BaseModel):
-    id: str
-    full_name: str
-    roles: list[str] = ['']
-    film_ids: list[str] = ['']
-
-
-class FilmShort(BaseModel):
-    id: str
-    title: str
-    imdb_rating: float
-
-
-@router.get('/', response_model=list[Person])
-async def get_all(person_service: PersonService = Depends(get_person_service)) -> list[Person]:
-    persons = await person_service.get_all()
+@router.get('/', response_model=list[Person], summary='Get all persons paginated')
+async def get_all(person_service: PersonService = Depends(get_person_service),
+                  page_number: int = 1,
+                  page_size: int = 50) -> list[Person]:
+    page_number = page_number if page_number > 0 else 1
+    _from = (page_number - 1) * page_size
+    persons = await person_service.get_all(_from, page_size)
     if not persons:
         return []
 
@@ -33,32 +26,40 @@ async def get_all(person_service: PersonService = Depends(get_person_service)) -
                    film_ids=person.film_ids) for person in persons]
 
 
-@router.get('/search', response_model=list[Person])
+@router.get('/search', response_model=list[Person], summary='Search in persons data by text field')
 async def search_persons(query: str,
                          page_number: int = 1,
                          page_size: int = 50,
                          person_service: PersonService = Depends(get_person_service)) -> list[Person]:
-    persons = await person_service.search_persons_by_name(query, page_number, page_size)
+    page_number = page_number if page_number > 0 else 1
+    _from = (page_number - 1) * page_size
+    persons = await person_service.search_persons_by_name(query, _from, page_size)
     if not persons:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='persons not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=http_errors.get('persons_not_found'))
     return persons
 
 
-@router.get('/{person_id}', response_model=Person)
+@router.get('/{person_id}', response_model=Person, summary='Get person by UUID')
 async def get_one(person_id: str, person_service: PersonService = Depends(get_person_service)) -> Person:
     person = await person_service.get_by_id(person_id)
     if not person:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=http_errors.get('person_not_found'))
     return Person(id=person.id, full_name=person.full_name, roles=person.roles, film_ids=person.film_ids)
 
 
-@router.get('/{person_id}/film', response_model=list[FilmShort])
+@router.get('/{person_id}/film', response_model=list[FilmShortModel],
+            summary='Get all films, in which person took place')
 async def get_films_by_pid(person_id: str,
-                           person_service: PersonService = Depends(get_person_service)) -> list[FilmShort]:
-    films = await person_service.get_films_by_person_id(person_id)
+                           page_number: int = 1,
+                           page_size: int = 50,
+                           person_service: PersonService = Depends(get_person_service)) -> list[FilmShortModel]:
+    page_number = page_number if page_number > 0 else 1
+    _from = (page_number - 1) * page_size
+
+    films = await person_service.get_films_by_person_id(person_id, _from, page_size)
     if not films:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
-    return [FilmShort(id=film.id, title=film.title, imdb_rating=film.imdb_rating) for film in films]
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=http_errors.get('films_not_found'))
+    return [FilmShortModel(id=film.id, title=film.title, imdb_rating=film.imdb_rating) for film in films]
 
 
 
