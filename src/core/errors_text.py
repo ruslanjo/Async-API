2604d6d1cf220f8045828ec8ff9fed6a6c9cdf3a
Ruslan Tarutin <tarutin.ruslan99@gmail.com>
2023-01-28 01:52:42 +0300

    Codereview 10: [added errors text]

---
+++ src/core/errors_text.py
@@ -0,0 +1,9 @@
+
+http_errors = {
+    'film_not_found': 'Film was not found',
+    'films_not_found': 'Films were not found',
+    'genre_not_found': 'Genre was not found',
+    'genres_not_found': 'Genres were not found',
+    'person_not_found': 'Person was not found',
+    'persons_not_found': 'Persons were not found'
+}
